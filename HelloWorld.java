54e12d651bbb33e00e616fe5dca615fe2836fbab
mmaattuu <martinvillandberg@gmail.com>
2016-09-30 10:20:06 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Martin Villandberg on 29/09/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
